9c3dd76ba3d3ca894fb6f294ce496e86f03af018
Mark Anthony <89271406+Marksva@users.noreply.github.com>
2022-07-22 15:10:53 -0300

    rota e metodos de Entrega

--- algalog-api/src/main/java/com/algaworks/algalog/domain/model/Entrega.java
+++ algalog-api/src/main/java/com/algaworks/algalog/domain/model/Entrega.java
@@ -1,7 +1,7 @@
 package com.algaworks.algalog.domain.model;
 
 import java.math.BigDecimal;
-import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
 
 import javax.persistence.Embedded;
 import javax.persistence.Entity;
@@ -9,12 +9,16 @@ import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+import javax.validation.groups.ConvertGroup;
+import javax.validation.groups.Default;
 
+import com.algaworks.algalog.domain.ValidationGroups;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonProperty.Access;
-
-import javax.persistence.Id;
 
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -31,12 +35,18 @@ public class Entrega {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long Id;
 	
+	@Valid
+	@ConvertGroup(from = Default.class, to = ValidationGroups.ClienteId.class )
+	@NotNull
 	@ManyToOne
-	private Cliente cliente; // queremos referenciar um cliente que já existe
+	private Cliente cliente; // usaremos o Id do cliente para referenciar um cliente que já existe. 
 	
+	@Valid
+	@NotNull
 	@Embedded
 	private Destinatario destinatario;
 	
+	@NotNull
 	private BigDecimal taxa;
 	
 	@JsonProperty(access = Access.READ_ONLY)
@@ -44,9 +54,9 @@ public class Entrega {
 	private StatusEntrega status;
 	
 	@JsonProperty(access = Access.READ_ONLY)
-	private LocalDateTime dataPedido;
+	private OffsetDateTime dataPedido;
 	
 	@JsonProperty(access = Access.READ_ONLY)
-	private LocalDateTime dataFinalizacao;
+	private OffsetDateTime dataFinalizacao;
 	
 }
